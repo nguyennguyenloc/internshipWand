453fb441c52b39e3019e8374d452d5853238ecfb
nguyennguyenloc <rephoto209@outlook.com>
2020-06-15 09:56:32 +0700

    React no width create-react-app - JSX

--- reactApp/App.js
+++ reactApp/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 class App extends Component {
     render() {
+        var hoho = {
+            fontSize: 100,
+            color: '#ff0000'
+        }
+        var i = 2;
         return (
             <div>
-                <h1>Hello World, chạy được rồi hu hu = ))</h1>
+                <h1 style={hoho}>Hello Word</h1>
+                <h2>Aloha</h2>
+                <p>Ok Google</p>
+                <p data-myattribute="somevalue">This is ReactJS program!!!!</p>
+                <hr>
+                </hr>
+                <h1>{1 + 1}</h1>
+                <hr></hr>
+                <h1>{i == 1 ? 'True!' : 'False'}</h1>
             </div>
         );
     }
