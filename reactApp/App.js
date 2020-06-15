d3f79bada4b05772ff69bf2134aa4525fba9e16e
nguyennguyenloc <rephoto209@outlook.com>
2020-06-15 09:28:23 +0700

    reactJS no with Create-react-app

---
+++ reactApp/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World, chạy được rồi hu hu = ))</h1>
+            </div>
+        );
+    }
+}
+export default App;
