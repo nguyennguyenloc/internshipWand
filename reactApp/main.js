2099860462f643eafb04490066c0120bd45d5f7f
nguyennguyenloc <rephoto209@outlook.com>
2020-06-16 08:08:00 +0700

    React no with create-react-app - Props Overview

--- reactApp/main.js
+++ reactApp/main.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 // import App from './App.js';
 import App from './App.jsx';
 
+//C1
+// ReactDOM.render(<App headerProp="Header header header..." contentProp="conten conten conten..." />, document.getElementById('app'));
+// export default App;
+
+//C2
 ReactDOM.render(<App />, document.getElementById('app'));
