ae34bbe065cebf95ebb4862b34b9fee5b64a9a6b
nguyennguyenloc <rephoto209@outlook.com>
2020-06-16 10:14:56 +0700

    ReactApp - Component Life Cycle

--- reactApp/App.js
+++ reactApp/App.js
@@ -1,24 +1,77 @@
 import React, { Component } from 'react';
-class App extends Component {
-    render() {
-        var hoho = {
-            fontSize: 100,
-            color: '#ff0000'
+// class App extends Component {
+//     render() {
+//         var hoho = {
+//             fontSize: 100,
+//             color: '#ff0000'
+//         }
+//         var i = 2;
+//         return (
+//             <div>
+//                 <h1 style={hoho}>Hello Word</h1>
+//                 <h2>Aloha</h2>
+//                 <p>Ok Google</p>
+//                 <p data-myattribute="somevalue">This is ReactJS program!!!!</p>
+//                 <hr>
+//                 </hr>
+//                 <h1>{1 + 1}</h1>
+//                 <hr></hr>
+//                 <h1>{i == 1 ? 'True!' : 'False'}</h1>
+//             </div>
+//         );
+//     }
+// }
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: 0
         }
-        var i = 2;
+        this.setNewNumber = this.setNewNumber.bind(this)
+    };
+    setNewNumber() {
+        this.setState({ data: this.state.data + 1 })
+    }
+    render() {
         return (
             <div>
-                <h1 style={hoho}>Hello Word</h1>
-                <h2>Aloha</h2>
-                <p>Ok Google</p>
-                <p data-myattribute="somevalue">This is ReactJS program!!!!</p>
-                <hr>
-                </hr>
-                <h1>{1 + 1}</h1>
-                <hr></hr>
-                <h1>{i == 1 ? 'True!' : 'False'}</h1>
+                <button onClick={this.setNewNumber}>Increment</button>
+                <Content myNumber={this.state.data}></Content>
             </div>
         );
     }
+}
+class Content extends React.Component {
+    componentWillMount() {
+        console.log('component will mount!')
+    }
+    componentDidMount() {
+        console.log('component Did Mount')
+    }
+    componentWillReceiveProps(newProps) {
+        console.log('component will ReceiveProps')
+    }
+    shouldComponentUpdate(newProps, newState) {
+        return true;
+    }
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Component Will Update!')
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Component did update')
+    }
+    componentWillUnmount() {
+        console.log('Component Will Unmount!')
+    }
+    render() {
+        return (
+            <div>
+                <h3>{this.props.myNumber}</h3>
+            </div>
+        );
+    }
+
 }
 export default App;
--- reactApp/main.js
+++ reactApp/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App.js';
-import App from './App.jsx';
+import App from './App.js';
+// import App from './App.jsx';
 
 //C1
 // ReactDOM.render(<App headerProp="Header header header..." contentProp="conten conten conten..." />, document.getElementById('app'));
@@ -9,3 +9,10 @@ import App from './App.jsx';
 
 //C2
 ReactDOM.render(<App />, document.getElementById('app'));
+
+//life_cycle
+setTimeout(
+    () => {
+        ReactDOM.unmountComponentAtNode(document.getElementById('app'));
+    }, 10000
+);
