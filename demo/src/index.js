3aee172a442e422fe9df6e2e2bb60c8e3409484b
nguyennguyenloc <rephoto209@outlook.com>
2020-06-18 10:29:26 +0700

    Render

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,26 +4,52 @@ import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-const user = {
-  name: "Nguyen Loc",
-  email: "nnloc123@gmail.com"
-};
+// const user = {
+//   name: "Nguyen Loc",
+//   email: "nnloc123@gmail.com"
+// };
 
 // const element = <h1>Hello World, my name is: {user.name}, and email: {user.email}</h1>
-const element = (
-  <div className="hello">
-    <h1>Hello World</h1>
-    <h2>Hello World, my name is: {user.name} and my email: {user.email}</h2>
-  </div>
-);
-var numbers = [1, 2, 3, 4];
+// const element = (
+//   <div className="hello">
+//     <h1>Hello World</h1>
+//     <h2>Hello World, my name is: {user.name} and my email: {user.email}</h2>
+//   </div>
+// );
+// var numbers = [1, 2, 3, 4];
 // var doubleNumbers = numbers.map(function (eachNumber) {
 //   return eachNumber * 2 + ", ";
 // });
-var doubleNumbers = numbers.map((eachNumber) => eachNumber * 3 + ", ");
-ReactDOM.render(<h1>doubleNumbers: {doubleNumbers}</h1>, document.getElementById('root'));
+// var doubleNumbers = numbers.map((eachNumber) => eachNumber * 3 + ", ");
+// ReactDOM.render(<h1>doubleNumbers: {doubleNumbers}</h1>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+//Render
+// c1:
+// var counter = 0;
+// setInterval(function () {
+//   counter += 1;
+//   console.log("updating....", counter)
+// }, 1000);
+// C2
+// var counter = 0;
+// function updaTimer() {
+//   counter += 1;
+//   console.log("hehehehe....", counter)
+// }
+// setInterval(updaTimer, 1000);
+
+function updateTime() {
+  const element = (
+    <div div >
+      <h1>Timer update example</h1>
+      <h2>Current time is: {new Date().toLocaleTimeString()}</h2>
+    </div >
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+setInterval(updateTime, 1000);
