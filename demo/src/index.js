394921c479ff90f819d045f2343b5a06ac24cf01
nguyennguyenloc <rephoto209@outlook.com>
2020-06-18 09:23:39 +0700

    JSX

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello World, HAHAHA</h1>, document.getElementById('root'));
+const user = {
+  name: "Nguyen Loc",
+  email: "nnloc123@gmail.com"
+};
+
+// const element = <h1>Hello World, my name is: {user.name}, and email: {user.email}</h1>
+const element = (
+  <div className="hello">
+    <h1>Hello World</h1>
+    <h2>Hello World, my name is: {user.name} and my email: {user.email}</h2>
+  </div>
+);
+var numbers = [1, 2, 3, 4];
+var doubleNUmbers = numbers.map(function (eachNumber) {
+  return eachNumber * 2 + ", ";
+});
+
+ReactDOM.render(<h1>doubleNumbers: {doubleNUmbers}</h1>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
