c821cf88e9f4628be57766d2b793cc64bf6ca16b
nguyennguyenloc <rephoto209@outlook.com>
2020-06-20 07:40:48 +0700

    Test_Net_core

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hellooo Worlddddd!");
+            var x = 5;
+            x = x + 10;
+            Console.WriteLine (x);
+        }
+    }
+}
