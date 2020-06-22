af135a5fb297baaabfa5a30af1f76347d66f6ef4
nguyennguyenloc <rephoto209@outlook.com>
2020-06-22 14:28:29 +0700

    DisplayItems

---
+++ todoapp/src/DisplayItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class DisplayItem extends React.Component {
+    constructor() {
+        super();
+        this.state = { done: false };
+    }
+    handleOnChange(event) {
+        var _done = !this.state.done;
+        this.setState({ done: _done });
+    }
+    render() {
+        var item = this.props.item;
+
+        return <li>
+            <input
+                checked={this.state.done}
+                onChange={this.handleOnChange.bind(this)}
+                type="checkbox"
+                style={{ fontSize: "x-large" }}
+            />
+            {item}
+            <button className="but1" onClick={this.props.handleDelete.bind(null, item)} >
+                Delete
+            </button>
+        </li>
+    };
+}
--- todoapp/src/DisplayList.js
+++ todoapp/src/DisplayList.js
@@ -1,34 +1,38 @@
 import React from 'react';
-
+import DisplayItem from './DisplayItem'
 export default class DisplayList extends React.Component {
-    constructor() {
-        super();
-        this.state = { done: false };
-    }
-    handleOnChange() {
-        var _done = !this.state.done;
-        this.setState({ done: _done });
-    }
+    // constructor() {
+    //     super();
+    //     this.state = { done: false };
+    // }
+    // handleOnChange() {
+    //     var _done = !this.state.done;
+    //     this.setState({ done: _done });
+    // }
     render() {
         return (
             <div>
                 <ul>
                     {this.props.items.map((item, i) => {
-                        return <li key={item}>
-                            {/* <input type="checkbox" style={{ fontSize: 'x-large' }} /> */}
-                            <input
-                                checked={this.state.done}
-                                onChange={this.handleOnChange.bind(this)}
-                                type="checkbox"
-                                style={{ fontSize: "x-large" }} />
-                            {item}
-                            <button className="but1" onClick={this.props.handleDelete.bind(null, item)} >
-                                Delete
-                            </button>
-                        </li>
+                        // return <li key={item}>
+                        //     <input
+                        //         checked={this.state.done}
+                        //         onChange={this.handleOnChange.bind(this)}
+                        //         type="checkbox"
+                        //         style={{ fontSize: "x-large" }} />
+                        //     {item}
+                        //     <button className="but1" onClick={this.props.handleDelete.bind(null, item)} >
+                        //         Delete
+                        //     </button>
+                        // </li>
+                        return <DisplayItem
+                            key={item}
+                            handleDelete={this.props.handleDelete.bind(null, item)}
+                            item={item} />
                     })}
                 </ul>
                 {/* <h1>alololololo</h1> */}
+                {/* <DisplayItem /> */}
             </div >
         )
     };
