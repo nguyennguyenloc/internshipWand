3ecd0d055ed9ada4145f563589c0503547d2e04d
nguyennguyenloc <rephoto209@outlook.com>
2020-06-27 15:54:42 +0700

    Prop

--- a/src/App.js
+++ a/src/App.js
@@ -1,34 +1,47 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import './App.jsx';
 
-function Name() {
+// function Name() {
+//   return (
+//     <div>
+//       <p>Name: Lộc</p>
+//       <p>Gender: Male</p>
+//     </div>
+//   );
+// }
+function Name(props) {
   return (
     <div>
-      <p>Name: Lộc</p>
-      <p>Gender: Male</p>
+      <p>Name: {props.name}</p>
+      <p>Gender: {props.gender}</p>
     </div>
   );
 }
 
-function Study() {
+// function Study() {
+//   return (
+//     <div>
+//       <p>IT</p>
+//       <p>VietNam</p>
+//     </div>
+//   );
+// }
+
+function Study(props) {
   return (
     <div>
-      <p>IT</p>
-      <p>VietNam</p>
+      <p>Study: {props.study}</p>
+      <p>Country: {props.country}</p>
     </div>
   );
 }
 function App() {
   return (
-    // <div className="App">
-    //   <h1>Heyyyyyyyyyyy</h1>
-    // </div>
     <div>
       <h1>hiuhihihiu</h1>
-      <Name />
-      <Study />
+      <Name name="Loc" gender="Male" />
+      <Study study="It" country="Viet Nam" />
     </div>
   );
 }
