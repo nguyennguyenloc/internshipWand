5aeebdd5b6119f8175db69b05dc54f54de77b815
nguyennguyenloc <rephoto209@outlook.com>
2020-07-03 09:08:26 +0700

    Edit + Delete

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+    handleEdit = () => {
+        this.props.openEditForm();
+        this.props.editTask(this.props.name, this.props.getIndexTask);
+    }
+    handleDelete = () => {
+        this.props.deleteTask(this.props.name)
+    }
     render() {
         return (
             <tr>
-                <td>{this.props.name}</td>
+                <td>
+                    {this.props.name}
+                </td>
+                <td>
+                    <button type="button" className="btn btn-outline-primary" onClick={this.handleEdit}> Edit</button>
+                </td>
+                <td>
+                    <button type="button" className="btn btn-outline-primary" onClick={this.handleDelete}> Delete</button>
+                </td>
             </tr>
+
         )
     };
 }
